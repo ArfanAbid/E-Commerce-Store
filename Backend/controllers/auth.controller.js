b6a54b5c922953a58891bd211bd8b96edc5042dc
Arfan Abid <iffionex111@gmail.com>
2024-09-17 01:31:02 +0500

    User Authentication with JWT done

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.models";
+import User from "../models/user.models.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import ApiError from "../utils/ApiError.js";
@@ -15,7 +15,6 @@ const generateTokens=(userId)=>{
             expiresIn:"15m"
         }
     );
-
     const refreshToken=jwt.sign(
         {
             userId
@@ -39,7 +38,6 @@ const setCookies=(res,accessToken,refreshToken)=>{
         maxAge:15*60*1000, // 15 minutes
     
     });
-
     res.cookie("refreshToken",refreshToken,{
         httpOnly:true, // prevent XSS attack
         secure:process.env.NODE_ENV==="production",
@@ -47,38 +45,72 @@ const setCookies=(res,accessToken,refreshToken)=>{
         maxAge:7*24*60*60*1000, // 7 days
 
     });
-
 }
 
-const registerUser=asyncHandler(async(req,res,next)=>{
-    const {name,email,password}=req.body;
-    if(!name || !email || !password){
-        throw new ApiError(400,"Please provide name, email and password");
-    }
-    const userExists=await User.findOne({email});
-    if(userExists){
-        throw new ApiError(409,"User already exists");
-    }
-    const user=await User.create({
-        name,
-        email,
-        password,
-    });
+// API controller for Register User
 
-    // authenticate user
-    const {accessToken,refreshToken}=generateTokens(user._id);
+const RegisterUser=asyncHandler(async(req,res,next)=>{
+    try {
+        const {name,email,password}=req.body;
+        if(!name || !email || !password){
+            throw new ApiError(400,"Please provide name, email and password");
+        }
+        const userExists=await User.findOne({email});
+        if(userExists){
+            throw new ApiError(409,"User already exists");
+        }
+        const user=await User.create({
+            name,
+            email,
+            password,
+        });
     
-    await storeRefreshToken(user._id,refreshToken);
-
-    setCookies(res,accessToken,refreshToken);
-
-    const createdUser=User.findById(user._id).select("-password");
-    if(!createdUser){
-        throw new ApiError(404,"User not found");
+        // authenticate user
+        const {accessToken,refreshToken}=generateTokens(user._id);
+        
+        await storeRefreshToken(user._id,refreshToken);
+    
+        setCookies(res,accessToken,refreshToken);
+    
+        const createdUser=User.findById(user._id).select("-password");
+        if(!createdUser){
+            throw new ApiError(404,"User not found");
+        }
+    
+        return res.status(201).json(new ApiResponse(201,createdUser,"User created successfully"));
+    } catch (error) {
+        console.log("Error in register user",error.message);
+        throw new ApiError(500,error.message);
     }
-
-    return res.status(201).json(new ApiResponse(201,createdUser,"User created successfully"));
 });
+
+// API controller for Login User
+
+const LoginUser=asyncHandler(async(req,res,next)=>{
+    try {
+        const {email,password}=req.body;
+        if(!email || !password){
+            throw new ApiError(400,"Please provide email and password");
+        }
+
+        const user=await User.findOne({email});
+        if(user && (await user.comparePassword(password))){
+            const {accessToken,refreshToken}=generateTokens(user._id);
+            await storeRefreshToken(user._id,refreshToken);
+            setCookies(res,accessToken,refreshToken);
+
+            return res.status(200).json(new ApiResponse(200,{_id,name,email,role},"User logged in successfully"));
+
+        }else{
+            throw new ApiError(401,"Invalid email or password");
+        }
+    } catch (error) {
+        console.log("Error in login user",error.message);
+        throw new ApiError(401,error.message);
+    }
+});
+
+// API controller for Logout User
 
 const LogoutUser=asyncHandler(async(req,res,next)=>{
     try {
@@ -94,11 +126,45 @@ const LogoutUser=asyncHandler(async(req,res,next)=>{
             return res.status(200).json(new ApiResponse(200,"User logged out successfully"));
         }
     } catch (error) {
+        console.log("Error in logout user",error.message);
         throw new ApiError(401,error.message);
     }
 
 })
 
-export default {
-    registerUser,
+// API controller to refresh the access token
+
+const RefreshToken=asyncHandler(async(req,res,next)=>{
+    const refresh=req.cookies.refreshToken;
+    if(!refresh){
+        throw new ApiError(401,"Unauthorized request");
+    }
+    const decoded=jwt.verify(refresh,process.env.REFRESH_TOKEN_SECRET);
+    if(!decoded){
+        throw new ApiError(401,"Please login to get access");
+    }
+    const storedRefreshToken=await redis.get(`refreshToken:${decoded.userId}`);
+    if(storedRefreshToken!==refresh){
+        throw new ApiError(401,"RefreshToken is expired or invalid");
+    }
+    const accessToken=jwt.sign({userId:decoded.userId},process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:"15m",
+    });
+
+    res.cookie("accessToken",accessToken,{
+        httpOnly:true, // prevent XSS attack
+        secure:process.env.NODE_ENV==="production",
+        sameSite:"strict",// prevent CSRF attack
+        maxAge:15*60*1000, // 15 minutes
+
+    });
+    return res.status(200).json(new ApiResponse(200,"Access token refreshed successfully"));
+});
+
+export {
+    RegisterUser,
+    LoginUser,
+    LogoutUser,
+    RefreshToken,
+
 }
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,10 +1,13 @@
 import express from "express";
-import RegisterUser from "../controllers/auth.controller.js";
+import {RegisterUser,LoginUser,LogoutUser,RefreshToken} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/register",RegisterUser);
+router.post("/login",LoginUser);
+router.post("/logout",LogoutUser);
+router.post("/refresh",RefreshToken);
 
-router.post("/register",RegisterUser.registerUser);
 
 
 export default router
